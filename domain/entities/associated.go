80f51518391c9a812cc636a9ecc10e9e146d4410
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-12-02 12:41:35 -0300

    implementacao da funcao SaveAssociated na camada repository

--- domain/entities/associated.go
+++ domain/entities/associated.go
@@ -2,7 +2,7 @@ package entities
 
 import "time"
 
-type Associate struct {
+type Associated struct {
 	ID              int       `json:"id"`
 	Name            string    `json:"name"`
 	CPF             string    `json:"cpf"`
@@ -11,7 +11,7 @@ type Associate struct {
 	DateOfBirth     time.Time `json:"date_of_birth"`    // data de nascimento
 	AssociationDate time.Time `json:"association_date"` // data de associacao
 	Address         string    `json:"address"`
-	DonationValue   float64   `json:"donation_value"` // valor de doacao
+	DonationValue   float64   `json:"donation_value"` // valor da doacao
 	PaymentDate     time.Time `json:"payment_date"`   // ultima data de pagamento
 	Status          bool      `json:"status"`
 }
