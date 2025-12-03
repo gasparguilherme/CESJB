47d840b844e6a4880c3bdf5270435b00836da988
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-12-03 11:39:19 -0300

    reeconstrucao do codigo para testes no postman

--- handlers/associated/service.go
+++ handlers/associated/service.go
@@ -2,10 +2,9 @@ package associated
 
 import (
 	"cesjb/domain/entities"
-	"time"
 )
 
 type Service interface {
-	CreateAssociated(name, cpf, email, tel string, date_of_birth, association_date time.Time,
-		address string, donation_value float64, payment_date time.Time, status bool) (*entities.Associated, error)
+	CreateAssociated(name, cpf, email, tel string, date_of_birth, association_date string,
+		address string, donation_value float64, payment_date string, status bool) (*entities.Associated, error)
 }
