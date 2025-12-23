cbac5facc7575e68df1f046fa79f270537c5be79
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-12-23 13:10:10 -0300

    refatorando regra de cpf valido

--- handlers/associate/validate/validate.go
+++ handlers/associate/validate/validate.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -42,12 +43,42 @@ func ValidateAssociate(
 
 	// valida cpf sem pontos
 	if len(cpf) != 11 {
-		return errors.New("o cpf deve conter exatamente 11 dígitos")
+		return errors.New("CPF deve ter exatamente 11 dígitos")
 	}
 
-	for _, r := range cpf {
-		if r < '0' || r > '9' {
-			return errors.New("o cpf deve conter apenas números")
+	// Verifica se todos os caracteres são números
+	for _, c := range cpf {
+		if c < '0' || c > '9' {
+			return errors.New("CPF deve conter apenas números")
+		}
+	}
+
+	// Evita CPFs com todos os dígitos iguais (ex: 11111111111)
+	igual := true
+	for i := 1; i < 11; i++ {
+		if cpf[i] != cpf[0] {
+			igual = false
+			break
+		}
+	}
+	if igual {
+		return errors.New("CPF inválido")
+	}
+
+	// Valida dígito verificador
+	for i := 9; i <= 10; i++ {
+		sum := 0
+		for j := 0; j < i; j++ {
+			num, _ := strconv.Atoi(string(cpf[j]))
+			sum += num * (i + 1 - j)
+		}
+		dig := (sum * 10) % 11
+		if dig == 10 {
+			dig = 0
+		}
+		expected, _ := strconv.Atoi(string(cpf[i]))
+		if dig != expected {
+			return errors.New("CPF inválido")
 		}
 	}
 
