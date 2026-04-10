8138bfe0fd473b9af156560097d0993288b74e5b
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-04-10 20:12:50 -0300

    fix: corrige cast explicito na query de total mensal

--- api/payment.go
+++ api/payment.go
@@ -1,15 +1,16 @@
 package api
- 
+
 import (
 	service "cesjb/domain/service/payment"
 	handler "cesjb/handlers/payment"
 	payment "cesjb/repository/postgresql/payment"
- 
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
- 
+
 func InitPayment(pool *pgxpool.Pool) handler.Handler {
 	r := payment.NewPostgresRepository(pool)
 	s := service.NewService(r)
 	h := handler.NewHandler(s)
-	
+	return h
+}
