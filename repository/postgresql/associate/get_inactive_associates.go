633b575ff7dbb71c8fa3285d1c42799e63820c75
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-04-13 19:00:12 -0300

    feat: adiciona query para buscar associados inativos

---
+++ repository/postgresql/associate/get_inactive_associates.go
@@ -0,0 +1,56 @@
+package associate
+
+import (
+	"cesjb/domain/entities"
+	"context"
+	"fmt"
+)
+
+func (r Repository) GetInactiveAssociates() ([]entities.Associate, error) {
+	query := `
+        SELECT 
+            id,
+            name,
+            cpf,
+            email,
+            tel,
+            date_of_birth,
+            association_date,
+            address,
+            status,
+			position
+        FROM associates
+		WHERE status = false
+        ORDER BY name ASC;
+    `
+
+	rows, err := r.connectionInstance.Query(context.TODO(), query)
+	if err != nil {
+		return nil, fmt.Errorf("executando query: %w", err)
+	}
+	defer rows.Close()
+
+	var associates []entities.Associate
+
+	for rows.Next() {
+		var a entities.Associate
+		err := rows.Scan(
+			&a.ID,
+			&a.Name,
+			&a.CPF,
+			&a.Email,
+			&a.Tel,
+			&a.DateOfBirth,
+			&a.AssociationDate,
+			&a.Address,
+			&a.Status,
+			&a.Position,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("scanning row: %w", err)
+		}
+		associates = append(associates, a)
+	}
+
+	return associates, nil
+}
